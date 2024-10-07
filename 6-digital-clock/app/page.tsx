015f881f057357ed277a73f6596e2e569512d5c0
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 01:00:28 +0500

    6 digital clock

---
+++ 6-digital-clock/app/page.tsx
@@ -0,0 +1,10 @@
+import DigitalClock from "@/components/ui/digital-clock";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <DigitalClock/>
+    </div>
+  );
+}
