43805d7ff804a018e5aae0bdb11fb117cd5354c1
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 01:15:44 +0500

    9 tip calculator

---
+++ 9-tip-calculator/app/page.tsx
@@ -0,0 +1,10 @@
+import TipCalculator from "@/components/ui/tip-calculator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <TipCalculator/>
+    </div>
+  );
+}
