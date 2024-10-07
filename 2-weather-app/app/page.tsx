c979e136ff8383d777469fb0243bfbf5072e8e94
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 00:30:53 +0500

    2 weather app

---
+++ 2-weather-app/app/page.tsx
@@ -0,0 +1,10 @@
+import WeatherApp from "@/components/ui/weather-app";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <WeatherApp/>
+    </div>
+   );
+}
