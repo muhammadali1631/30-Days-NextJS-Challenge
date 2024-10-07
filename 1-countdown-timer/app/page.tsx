5ba59acab65b0bcb24d4bf60b27d9385c2621a00
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 00:21:47 +0500

    1 countdown timer

---
+++ 1-countdown-timer/app/page.tsx
@@ -0,0 +1,10 @@
+import Countdown from "@/components/ui/count-down";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Countdown/>
+    </div>
+    );
+}
