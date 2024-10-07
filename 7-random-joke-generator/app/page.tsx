292f5c69459c40cca316f1162de8957ed7430248
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 01:06:20 +0500

    7 random joke

---
+++ 7-random-joke-generator/app/page.tsx
@@ -0,0 +1,10 @@
+import RandomJoke from "@/components/ui/random-joke";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <RandomJoke/>
+    </div>
+  );
+}
