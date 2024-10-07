a8acbce79462c4b37620f273253ca6dd2ddf936a
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 00:46:32 +0500

    4 number guessing

---
+++ 4-number-guessing/app/page.tsx
@@ -0,0 +1,10 @@
+import NumberGuessing from "@/components/ui/number-guessing";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <NumberGuessing/>
+    </div>
+  );
+}
