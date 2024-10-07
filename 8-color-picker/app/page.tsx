4440f727f4029b5292b22b437bb96f818bdcf390
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 01:10:43 +0500

    8 color picker

---
+++ 8-color-picker/app/page.tsx
@@ -0,0 +1,10 @@
+import ColorPicker from "@/components/ui/color-picker";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ColorPicker/>
+    </div>
+  );
+}
