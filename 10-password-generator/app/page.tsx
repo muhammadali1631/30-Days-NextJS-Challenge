c3cb98f6073918625bc8c51d37fbd38cc0efeebf
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 01:28:15 +0500

    10 password generator

---
+++ 10-password-generator/app/page.tsx
@@ -0,0 +1,10 @@
+import PasswordGenerator from "@/components/ui/password-generator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <PasswordGenerator/>
+    </div>
+  );
+}
