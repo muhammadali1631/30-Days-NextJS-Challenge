ebbcd826ab56b3973274ecef59cfa23ae5e16397
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 00:37:46 +0500

    3 birthday wish

---
+++ 3-birthday-wish/app/page.tsx
@@ -0,0 +1,10 @@
+import BirthdayWish from "@/components/ui/birthday-wish";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <BirthdayWish/>
+    </div>
+      );
+}
