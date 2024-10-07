741b89c0ab870640e62efe17d3a2c24fb1fdc2f1
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 00:54:04 +0500

    5 calculator

---
+++ 5-calculator/app/page.tsx
@@ -0,0 +1,10 @@
+import Calculator from "@/components/ui/calculator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Calculator/>
+    </div>
+  );
+}
