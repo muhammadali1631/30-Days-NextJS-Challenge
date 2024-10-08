cf363ba33b0eadeee2a2987492c65960bdc5e225
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 17:54:07 +0500

    20 notes app

---
+++ 20-notes-app/app/page.tsx
@@ -0,0 +1,10 @@
+import NotesApp from "@/components/ui/notes-app";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <NotesApp />
+    </div>
+  );
+}
