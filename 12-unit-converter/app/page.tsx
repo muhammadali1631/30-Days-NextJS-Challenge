a45394449cb5ed84061413b12dadbc0dec906c79
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 15:27:36 +0500

    12 unit converter

---
+++ 12-unit-converter/app/page.tsx
@@ -0,0 +1,10 @@
+import UnitConverter from "@/components/ui/unit-converter";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <UnitConverter />
+    </div>
+  );
+}
