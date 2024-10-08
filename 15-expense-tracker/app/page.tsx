3f50f01a462f4ccccbc57c7e449a60b4b171da62
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 16:13:06 +0500

    15 expense tracker

---
+++ 15-expense-tracker/app/page.tsx
@@ -0,0 +1,10 @@
+import ExpenseTracker from "@/components/ui/expense-tracker";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ExpenseTracker />
+    </div>
+  );
+}
