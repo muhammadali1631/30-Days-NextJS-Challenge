ca08ef5957b2d4b279ac6dd5ac173dd92a034247
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 17:39:53 +0500

    18 currency converter

---
+++ 18-currency-converter/app/page.tsx
@@ -0,0 +1,10 @@
+import CurrencyConverter from "@/components/ui/currency-converter";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <CurrencyConverter />
+    </div>
+    );
+}
