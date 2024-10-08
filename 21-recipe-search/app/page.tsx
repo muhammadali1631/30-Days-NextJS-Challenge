19efdc98c2d1d0bb2f205cf7be000a8a2ebb848d
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 17:59:46 +0500

    21 recipe search

---
+++ 21-recipe-search/app/page.tsx
@@ -0,0 +1,10 @@
+import RecipeSearch from "@/components/ui/recipe-search";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <RecipeSearch />
+    </div>
+  );
+}
