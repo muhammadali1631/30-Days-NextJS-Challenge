056630f752dc6b01a71e575aeea757412b8c8a2c
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 12:58:03 +0500

    11 BMI Calculator

---
+++ 11-bmi-calculator/app/page.tsx
@@ -0,0 +1,10 @@
+import BmiCalculator from "@/components/ui/bmi-calculator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <BmiCalculator />
+    </div>
+  );
+}
