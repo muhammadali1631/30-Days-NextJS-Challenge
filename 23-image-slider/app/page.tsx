4000b5af750b2d60a487045325dbe55831be69e8
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 19:27:56 +0500

    23 image slider

---
+++ 23-image-slider/app/page.tsx
@@ -0,0 +1,10 @@
+import ImageSlider from "@/components/ui/image-slider";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ImageSlider />
+    </div>
+  );
+}
