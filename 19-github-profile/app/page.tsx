ddfaa9b69dbb53e6535357108c71bcb418a4c9a9
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 17:48:55 +0500

    19 github profile viewer

---
+++ 19-github-profile/app/page.tsx
@@ -0,0 +1,10 @@
+import GithubProfile from "@/components/ui/github-profile";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <GithubProfile />
+    </div>
+  );
+}
