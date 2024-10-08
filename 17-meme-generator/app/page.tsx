a19a0972976d8a49d237cd1bcb40d78d67950c34
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 16:42:19 +0500

    17 meme generator

---
+++ 17-meme-generator/app/page.tsx
@@ -0,0 +1,10 @@
+import MemeGenerator from "@/components/ui/meme-generator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <MemeGenerator />
+    </div>
+  );
+}
