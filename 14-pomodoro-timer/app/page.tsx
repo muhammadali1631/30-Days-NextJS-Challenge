1af16cc8ddb07ac61aafe55ae73a9e5781b32ea9
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 16:02:33 +0500

    14 pomodoro timer

---
+++ 14-pomodoro-timer/app/page.tsx
@@ -0,0 +1,10 @@
+import PomodoroTimer from "@/components/ui/pomodoro-timer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <PomodoroTimer />
+    </div>
+  );
+}
