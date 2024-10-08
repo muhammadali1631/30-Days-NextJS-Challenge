b97b9fe01a38bce812640bc660816f639e7d4f20
muhammadali1631 <160423266+muhammadali1631@users.noreply.github.com>
2024-10-08 15:37:55 +0500

    13 html previewer

---
+++ 13-html-preview/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import HTMLPreviewer from "@/components/ui/html-previewer";
+export default function Home() {
+  return (
+    <div>
+      <HTMLPreviewer />
+    </div>
+  );
+}
